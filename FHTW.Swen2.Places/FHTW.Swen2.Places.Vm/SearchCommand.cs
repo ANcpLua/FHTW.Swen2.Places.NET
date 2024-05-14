862296a36aca2afd139690ca7c9bc149c47b4b0c
Alexander Lang <alexander.lang@orf.at>
2024-05-14 17:38:16 +0200

    0.0.5 implemented search view and view model.

---
+++ FHTW.Swen2.Places/FHTW.Swen2.Places.Vm/SearchCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using SWEN2.Places.Model;
+
+namespace FHTW.Swen2.Places.Vm
+{
+    /// <summary>This class implements the search command.</summary>
+    internal class SearchCommand: ICommand
+    {
+        /// <summary>Parent view model.</summary>
+        internal MainViewModel _Parent;
+
+
+        /// <summary>Creates a new instance of this class.</summary>
+        /// <param name="parent">Parent view model.</param>
+        internal SearchCommand(MainViewModel parent)
+        {
+            _Parent = parent;
+            _Parent.PropertyChanged += (sender, e) => 
+            { 
+                if(e.PropertyName == nameof(_Parent.SearchExpression)) 
+                { 
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty); 
+                }
+            };
+        }
+
+
+
+        /// <summary>Occurs when the CanExecute property has changed.</summary>
+        public event EventHandler? CanExecuteChanged;
+
+
+        /// <summary>Determines if a command can be executed.</summary>
+        /// <param name="parameter">Parameter.</param>
+        /// <returns>Returns TRUE if the command can be executed, otherwise returns FALSE.</returns>
+        public bool CanExecute(object? parameter)
+        {
+            return !string.IsNullOrWhiteSpace(_Parent.SearchExpression);
+        }
+
+
+        /// <summary>Executes the command.</summary>
+        /// <param name="parameter">Parameter.</param>
+        public void Execute(object? parameter)
+        {
+            _Parent.ResultPage.SearchResults.Clear();
+
+            foreach(Place i in Root.Context.SearchPlaces(_Parent.SearchExpression))
+            {
+                _Parent.ResultPage.SearchResults.Add(new(_Parent.ResultPage, i));
+            }
+            _Parent.ShowSearchResults();
+        }
+    }
+}
---
+++ FHTW.Swen2.Places/FHTW.Swen2.Places.Vm/SearchResultData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SWEN2.Places.Model;
+
+
+
+namespace FHTW.Swen2.Places.Vm
+{
+    /// <summary>This class represents search results for the view model.</summary>
+    internal class SearchResultData
+    {
+        /// <summary>Parent view model.</summary>
+        internal ResultPageViewModel _Parent;
+
+
+        /// <summary>Creates a new instance of this class.</summary>
+        /// <param name="parent">Parent view model.</param>
+        /// <param name="place">Place.</param>
+        internal SearchResultData(ResultPageViewModel parent, Place place)
+        {
+            _Parent = parent;
+            Place = place;
+        }
+
+
+        /// <summary>Gets the place represented by this search result.</summary>
+        public Place Place
+        {
+            get; private init;
+        }
+
+
+        /// <summary>Gets the place name.</summary>
+        public string Name
+        {
+            get { return Place.Name; }
+        }
+
+
+        /// <summary>Gets the place description.</summary>
+        public string Description
+        {
+            get { return Place.Description; }
+        }
+    }
+}
